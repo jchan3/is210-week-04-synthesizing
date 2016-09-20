101c69062c0c9aadd951159f6f631cb426f834bb
jchan3 <joechan2014@gmail.com>
2016-09-20 23:58:46 +0000

    commit

---
+++ task_01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module provides a function that converts Fahrenheit to Celsius."""
+
+
+import decimal
+
+
+def fahrenheit_to_celsius(degrees):
+    """Converts a parameter from Fahrenheit to Celsius.
+
+    Args:
+        degrees(int): A Fahrenheit variable.
+
+    Returns:
+        decimal: A celsius value.
+
+    Examples:
+
+        >>> fahrenheit_to_celsius(212)
+        Decimal('100')
+
+    """
+    return decimal.Decimal(((degrees - 32) * 5) / 9)
